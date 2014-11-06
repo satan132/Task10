3e2ccdce0f8cd8b040b380938d8dbaa655c7d203
konstanchuk <vova.konstanchuk@gmail.com>
2014-11-06 22:26:12 +0200

    test

--- js/tasks.js
+++ js/tasks.js
@@ -2,6 +2,7 @@ function TasksViewModel(desc) {
     var self = this;
 
     self.Desc = '';
+    self.Date = '123434343';
 
     self.status = ['not_started', 'in_progress', 'ready_test', 'completed'];
 
@@ -14,21 +15,26 @@ function TasksViewModel(desc) {
         if (self.Desc.length > 0) {
             if(type == 'not_started') {
                 self.not_started.push({
-                    Desc: ko.observable(self.Desc)
+                    Desc: ko.observable(self.Desc),
+                    Date: ko.observable(self.Date)
                 });
             } else if(type == 'in_progress') {
                 self.in_progress.push({
-                    Desc: ko.observable(self.Desc)
+                    Desc: ko.observable(self.Desc),
+                    Date: ko.observable(self.Date)
                 });
             } else if(type == 'ready_test') {
                 self.ready_test.push({
-                    Desc: ko.observable(self.Desc)
+                    Desc: ko.observable(self.Desc),
+                    Date: ko.observable(self.Date)
                 });
             } else if(type == 'completed') {
-                self.completed.push({
-                    Desc: ko.observable(self.Desc)
+                self.completed.push( {
+                    Desc: ko.observable(self.Desc),
+                    Date: ko.observable(self.Date)
                 });
             }
+            self.Desc = '';
         }
     }
 }
