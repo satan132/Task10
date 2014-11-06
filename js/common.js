6a4f5582da057f788ba368a06fcafe6c64ef18fc
konstanchuk <vova.konstanchuk@gmail.com>
2014-11-06 16:55:09 +0200

    test

--- js/common.js
+++ js/common.js
@@ -1,6 +1,13 @@
 function pageSize() {
-    $('#content').css('height', $(window).height()-41 + 'px');
-    $('#btasks').css('width', $(window).width()-31 + 'px');
+    function size() {
+        $('#content').css('height', $(window).height()-41 + 'px');
+        $('#btasks').css('width', $(window).width()-31 + 'px');
+        $('#edit').css('height', $(window).height() + 'px');
+    }
+
+    size();
+
+    $(window).resize(size);
 }
 
 
