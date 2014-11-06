e15a09ca419af4ffe2668cc1b5cf7832fb1684dd
konstanchuk <vova.konstanchuk@gmail.com>
2014-11-06 22:16:46 +0200

    test

--- js/tasks.js
+++ js/tasks.js
@@ -11,10 +11,21 @@ function TasksViewModel(desc) {
     self.completed = ko.observableArray([]);
 
     self.add = function(type) {
-        alert(self.Desc);
         if (self.Desc.length > 0) {
             if(type == 'not_started') {
                 self.not_started.push({
+                    Desc: ko.observable(self.Desc)
+                });
+            } else if(type == 'in_progress') {
+                self.in_progress.push({
+                    Desc: ko.observable(self.Desc)
+                });
+            } else if(type == 'ready_test') {
+                self.ready_test.push({
+                    Desc: ko.observable(self.Desc)
+                });
+            } else if(type == 'completed') {
+                self.completed.push({
                     Desc: ko.observable(self.Desc)
                 });
             }
